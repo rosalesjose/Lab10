869bb0b45a55c7a16997aafb96c086505d03f699
Jose <jrosales0001@gmail.com>
2018-02-01 16:14:49 -0500

    added movie list

--- Lab10/Movie.cs
+++ Lab10/Movie.cs
@@ -6,10 +6,33 @@ using System.Threading.Tasks;
 
 namespace Lab10
 {
-
-
     class Movie
     {
-    }
+        private string title;
+        private string category;
 
+        public string Title
+        {
+            set { title = value; } 
+
+            get { return title; } 
+        }
+
+        public string Category
+        {
+            set { category = value; } 
+
+            get { return category; } 
+        }               
+
+        public void PrintTitle()
+        {
+            Console.WriteLine(Title);
+        }
+
+        public void PrintCategory()
+        {
+            Console.WriteLine(Category);
+        }
+    }
 }
--- Lab10/Program.cs
+++ Lab10/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,53 @@ namespace Lab10
         static void Main(string[] args)
         {
             Console.Title = "Movie List Application";
+
+            List<Movie> MovieList = Movies();
+
+            //menu of genres
+
+            Console.Write("Enter a genre: ");
+
+            string Choice = Console.ReadLine();
+
+            if (Choice == "Superhero")
+            {
+                foreach (Movie item in MovieList)
+                {
+
+                }
+            }
+            else if (Choice == "Disney")
+            {
+
+            }
+            else if (Choice == "Crime")
+            {
+
+            }
+        }
+
+        private static List<Movie> Movies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Title = "Superman", Category = "Superhero" },
+                new Movie { Title = "Batman", Category = "Superhero" },
+                new Movie { Title = "Wonderwoman", Category = "Superhero" },
+                new Movie { Title = "Thor", Category = "Superhero" },
+                new Movie { Title = "Ironman", Category = "Superhero" },
+                new Movie { Title = "Inside Out", Category = "Disney" },
+                new Movie { Title = "101 Dalmations", Category = "Disney" },
+                new Movie { Title = "Up", Category = "Disney" },
+                new Movie { Title = "Mulan", Category = "Disney" },
+                new Movie { Title = "Toy Story", Category = "Disney" },
+                new Movie { Title = "The Godfather", Category = "Crime" },
+                new Movie { Title = "Goodfellas", Category = "Crime" },
+                new Movie { Title = "City of God", Category = "Crime" },
+                new Movie { Title = "Gold", Category = "Crime" },
+                new Movie { Title = "Wolf of Wall Street", Category = "Crime" },
+
+            };
         }
     }
 }
